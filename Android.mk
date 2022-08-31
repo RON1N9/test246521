10e2d794f1ef437e902fd370c5b9569f4ceb5818
RON1N9 <89311941+RON1N9@users.noreply.github.com>
2022-08-31 19:43:38 +0600

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 ifneq ($(filter j7elte,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
-
+TW_THEME := portrait_hdpi #most likely what you need
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
